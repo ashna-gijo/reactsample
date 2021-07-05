b3d47462144eb13862b8b3aea38042dfae614456
ashna <ashnagijo@gmail.com>
2021-07-05 21:19:40 +0530

    changes

--- src/LoginFunctional.js
+++ src/LoginFunctional.js
@@ -1,14 +1,35 @@
 import{useState}from 'react';
+import{useHistory}from 'react-router-dom';
 
 function LoginFunctional(){
+    const history= useHistory;
     const[username,setUsername]=useState("");
     const[password,setPassword]=useState("");
         
+
+   const usernameChange=(event)=>{
+       setUsername(event.target.value);
+        
+    }
+    const passwordChange=(event)=>{
+        setPassword(event.target.value);
+        
+    }
+
+    const onSubmitClick=(event)=>{
+        event.preventDefault();
+        if(username=="user1"&&password=="123"){
+            alert("success");
+            history.push("/home")
+        }else{
+            alert("Invalid Credentials")
+        }
+        }
         return <div className="row">
             <h1>Login{username}</h1>
             <div className="col-md-6">
             
-            <form>
+            <form onSubmit={onSubmitClick}>
             <label>Username</label>
             <input className="form-control" type="text" name="username" />
             <label>Password</label>
@@ -20,4 +41,4 @@ function LoginFunctional(){
             </div>
     }
 
-    export default LoginFunctional;
+    export default LoginFunctional(Login);
